f497b5bb955bc5c5f2292d1daf273a3323340cbc
ysgorin <ysgorin@gmail.com>
2023-02-20 20:27:32 -0500

    added employees table schema

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,15 @@
+-- Employees Table Schema
+CREATE TABLE Employees (
+    Emp_no INT,
+    Emp_title_id VARCHAR,
+    Birth_date VARCHAR,
+    First_name VARCHAR,
+    Last_name VARCHAR,
+    Sex CHAR(1),
+    Hire_date VARCHAR
+):
+
+-- Check data import
+SELECT *
+FROM Employees
+

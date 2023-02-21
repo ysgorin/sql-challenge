deddee2a4b592935bd545caf890b27f50c01dc25
ysgorin <ysgorin@gmail.com>
2023-02-20 20:55:20 -0500

    started department employees table schema and added semi-colons

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -7,19 +7,23 @@ CREATE TABLE Employees (
     Last_name VARCHAR,
     Sex CHAR(1),
     Hire_date VARCHAR
-):
+);
 
 -- Check data import
 SELECT *
-FROM Employees
+FROM Employees;
 
 -- Departments Table Schema
 CREATE TABLE Departments (
     Dept_no VARCHAR
     Dept_name VARCHAR
-)
+);
 
 -- Check data import
 SELECT *
-FROM Departments
+FROM Departments;
 
+-- Department Employees Table Scheme
+CREATE TABLE Department_Employees (
+
+);
